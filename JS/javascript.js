bdb065020592289f0a47166b72a222294ab6769b
Affonso Mesquita <102181682+Affmesquita@users.noreply.github.com>
2023-09-27 17:08:22 -0300

    javascript, medeia query e overlay

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,6 +1,7 @@
-const nav = document.getElementById('nav');
+    const nav = document.getElementById('nav');
     const hamburguerIcon = document.getElementById('hamburguer-icon');
     const closeIcon = document.getElementById('close-icon');
+    const overlay = document.querySelector('.overlay');
 
     hamburguerIcon.addEventListener('click', () => {
       nav.classList.add('show');
@@ -13,3 +14,12 @@ const nav = document.getElementById('nav');
       closeIcon.style.display = 'none';
       hamburguerIcon.style.display = 'block';
     });
+
+    hamburguerIcon.addEventListener('click', () => {
+      overlay.style.display = 'block';
+    });
+
+    closeIcon.addEventListener('click', () => {
+      overlay.style.display = 'none';
+    });
+
